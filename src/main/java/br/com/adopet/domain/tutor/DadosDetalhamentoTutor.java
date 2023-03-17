03db6e81be93aa21b304788002d20db5584a64c4
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2023-03-17 08:50:24 -0300

    Fix: corrigindo erro de compilação

--- src/main/java/br/com/adopet/domain/tutor/DadosDetalhamentoTutor.java
+++ src/main/java/br/com/adopet/domain/tutor/DadosDetalhamentoTutor.java
@@ -1,2 +1,7 @@
-package br.com.adopet.domain.tutor;public record DadosDetalhamentoTutor() {
+package br.com.adopet.domain.tutor;
+
+public record DadosDetalhamentoTutor(Long id, String nome, String email, String foto, String telefone, String cidade, String sobre) {
+    public DadosDetalhamentoTutor(Tutor tutor){
+        this(tutor.getId(), tutor.getNome(), tutor.getEmail(), tutor.getFoto(), tutor.getTelefone(), tutor.getCidade(), tutor.getSobre());
+    }
 }
